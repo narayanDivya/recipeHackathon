bcad311e2973a4dca0fbd2c9058dd4f1f513e703
=Paramita <=Paramita.mahata30@gmail.com>
2023-04-24 17:59:22 -0400

    added ingredients in PCOS properties file

---
+++ src/main/java/recipeHackathon/PCOSTraverse.java
@@ -0,0 +1,41 @@
+package recipeHackathon;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import recipeHackathon.Helper.ComorbidityScraper;
+import recipeHackathon.Model.Comorbidity;
+
+public class PCOSTraverse extends ComorbidityTraverser {
+	
+	@BeforeTest
+	public void setUp() {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		scraper = new ComorbidityScraper(driver); 		
+
+	}
+
+	public void LoadRecipesToExcel() throws Exception {
+		LoadRecipeToExcel();
+	}
+	
+	@Override
+	public String GetMorbidityParentPage() {
+		driver.get("https://tarladalal.com/");
+		driver.findElement(By.xpath("//div[contains(text(),'RECIPES')]")).click();
+		driver.findElement(By.id("ctl00_cntleftpanel_ttlhealthtree_tvTtlHealtht335")).click();
+		String pageURL=driver.getCurrentUrl();
+		
+		return pageURL;
+	}
+
+	@Override
+	public Comorbidity SetCoMorbidity() {
+		return Comorbidity.PCOS;
+		
+	}	
+}
